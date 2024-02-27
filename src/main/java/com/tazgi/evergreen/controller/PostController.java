6dd926a9e3a4d95d50f88129d853846bd3ac1ad0
Jayanth Chennupati <40850732+jayanth122@users.noreply.github.com>
2024-02-27 17:10:07 -0500

    Added github and google oAuth2

--- src/main/java/com/tazgi/evergreen/controller/PostController.java
+++ src/main/java/com/tazgi/evergreen/controller/PostController.java
@@ -4,6 +4,7 @@ import com.tazgi.evergreen.ItemRepository;
 import com.tazgi.evergreen.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,4 +19,9 @@ public class PostController {
     public List<Item> getAllItems(){
         return repo.findAll();
     }
+
+//    @GetMapping("/")
+//    public String login(){
+//        return "login successful";
+//    }
 }
